a899ed3e3d549a8cf6a8479d722854d5c52028fd
Cristian Nirones <Cristian.Nirones@endava.com>
2019-10-17 10:48:20 +0300

    4

---
+++ DIWorkshop/DIWorkshop.WebApi.Homework/Controllers/DriverController.cs
@@ -0,0 +1,22 @@
+﻿using DIWorkshop.Managers;
+using DIWorkshop.Persistence;
+using System.Web.Http;
+
+namespace DIWorkshop.WebApi.Homework.Controllers
+{
+	public class DriverController : ApiController
+    {
+		private readonly DriverManager _driverManager;
+
+		public DriverController()
+		{
+			_driverManager = new DriverManager(new DriverRepository(new DbContext()));
+		}
+
+		[Route("driver/{id}")]
+		public IHttpActionResult GetDriver(int id)
+		{
+			return Ok(_driverManager.GetDriver(id));
+		}
+	}
+}
---
+++ DIWorkshop/DIWorkshop.WebApi.Homework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DIWorkshop.WebApi.Homework.WebApiApplication" Language="C#" %>
